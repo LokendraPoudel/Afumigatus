0111ca4f8e2b204e9eae52585cf9d1d144bb4cc3
henriquedeassis <deassis.info@gmail.com>
2019-03-22 13:54:17 -0400

    minor change in Model.interact method. Not tested!

--- Test/edu/uchc/interactable/test_transferrin.py
+++ Test/edu/uchc/interactable/test_transferrin.py
@@ -4,11 +4,11 @@ from edu.uchc.interactable.Cells import Macrophage
 
 class TestTransferrin(TestCase):
     def test_interact(self):
-        m = Macrophage(None, None, None)
-        tfbi = Molecule(0)
+        m = Macrophage(50)
         iron = Iron(10)
-        tf = Transferrin(tfbi, 10)
-        print((iron.get(), tf.get(), tf.tfbi.get()))
+        tf = Transferrin(10, 10)
+        print((iron.get(), tf.getTf(), tf.getTfBI()))
         print(iron.interact(tf))
-        print((iron.get(), tf.get(), tf.tfbi.get()))
+        print((iron.get(), tf.getTf(), tf.getTfBI()))
         print(m.interact(tf))
+        print((iron.get(), tf.getTf(), tf.getTfBI()))
