8abbe82f1e1b45efc31a261670c00e4068251992
henriquedeassis <deassis.info@gmail.com>
2019-03-22 13:53:51 -0400

    minor change in Model.interact method. Not tested!

--- Test/edu/uchc/interactable/test_afumigatus.py
+++ Test/edu/uchc/interactable/test_afumigatus.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 from edu.uchc.interactable.Cells import *
+#from edu.uchc.interactable.Molecules import *
 
 class TestAfumigatus(TestCase):
     def test_elongate(self):
         print("TEST ELONGATION")
-        a1 = Afumigatus(None, None, None, status=Afumigatus.HYPHAE)
+        a1 = Afumigatus(status=Afumigatus.HYPHAE)
         a1.boolean_network[Afumigatus.LIP] = 1
         a2 = a1.elongate()
         print(a1)
@@ -13,7 +14,7 @@ class TestAfumigatus(TestCase):
 
     def test_branch(self):
         print("TEST BRANCH")
-        a1 = Afumigatus(None, None, None, status=Afumigatus.HYPHAE)
+        a1 = Afumigatus(status=Afumigatus.HYPHAE)
         for i in range(10):
             a1.branchable = True
             a1.boolean_network[Afumigatus.LIP] = 1
@@ -25,37 +26,29 @@ class TestAfumigatus(TestCase):
 
     def test_processBooleanNetwork(self):
         print("TEST PROCESS_BOOLEAN_NETWORK")
-        #def __init__(self, iron, tafc, tafcbi, ironPool=0, status=0, isRoot=True):
-        iron   = Molecule(100)
-        tafc   = Molecule(100)
-        tafcbi = Molecule(100)
-        a = Afumigatus(iron, tafc, tafcbi)
-        a.has_iron = True
-        a.has_tafcbi = True
+        a = Afumigatus()
+        a.boolean_network[Afumigatus.Fe] = 1
+        a.boolean_network[Afumigatus.TAFCBI] = 1
 
         print(a.boolean_network)
-        print((iron.get(), tafc.get(), tafcbi.get()))
         a.process_boolean_network()
         print(a.boolean_network)
-        print((iron.get(), tafc.get(), tafcbi.get()))
         print()
 
     def test_secreteTAFC(self):
         print("TEST SECRET_TAFC")
-        iron = Molecule(100)
-        tafc = Molecule(100)
-        tafcbi = Molecule(100)
-        a = Afumigatus(iron, tafc, tafcbi)
+        tafc = TAFC(0, 0)
+        a = Afumigatus()
         a.boolean_network[Afumigatus.TAFC] = 1
 
-        print(tafc.get())
-        a.secrete_tafc()
-        print(tafc.get())
+        print(tafc.getTafc())
+        a.secrete(tafc)
+        print(tafc.getTafc())
         print()
 
     def test_updateStatus(self):
         print("TEST UPDATE_STATUS")
-        a = Afumigatus(None, None, None)
+        a = Afumigatus()
         while(a.status != Afumigatus.HYPHAE):
             a.update_status()
             print(a.status)
@@ -63,7 +56,7 @@ class TestAfumigatus(TestCase):
 
     def test_diffuseIron(self):
         print("TEST DIFFUSE_IRON")
-        a = Afumigatus(None, None, None, 1024, status=Afumigatus.HYPHAE)
+        a = Afumigatus(1024, status=Afumigatus.HYPHAE)
         a0 = a
         list = []
         for i in range(10):
@@ -83,21 +76,18 @@ class TestAfumigatus(TestCase):
 
     def test_interact(self):
         print("TEST INTERACT")
-        m = Macrophage(None, None, None)
-        tf = Transferrin(None, 100)
+        m = Macrophage()
+        tf = Transferrin(100, 100)
         iron = Iron(100)
-        tfbi = TransferrinBI(100)
-        af = Afumigatus(None, None, None, status = Afumigatus.SWELLING_CONIDIA)
+        af = Afumigatus(status = Afumigatus.SWELLING_CONIDIA)
 
-        print((tf.get(), tfbi.get(), iron.get()))
+        print((tf.getTf(), tf.getTfBI(), iron.get()))
         print(tf.interact(af))
-        print((tf.get(), tfbi.get(), iron.get(), af.has_iron))
-        print(tfbi.interact(af))
-        print((tf.get(), tfbi.get(), iron.get(), af.has_iron))
+        print((tf.getTf(), tf.getTfBI(), iron.get(), af.boolean_network[Afumigatus.Fe]))
         print(iron.interact(af))
-        print((tf.get(), tfbi.get(), iron.get(), af.has_iron))
+        print((tf.getTf(), tf.getTfBI(), iron.get(), af.boolean_network[Afumigatus.Fe]))
 
-        while(not m.attached_fungus):
+        while(m.boolean_network[Macrophage.Bglucan] != 1):
             print(m.interact(af))
             print(m.attached_fungus)
 
