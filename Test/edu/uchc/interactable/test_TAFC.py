666028d103ac9a810477357c266487545d63e5e8
henriquedeassis <deassis.info@gmail.com>
2019-03-22 13:54:12 -0400

    minor change in Model.interact method. Not tested!

--- Test/edu/uchc/interactable/test_TAFC.py
+++ Test/edu/uchc/interactable/test_TAFC.py
@@ -3,42 +3,35 @@ from edu.uchc.interactable.Cells import *
 
 class TestTAFC(TestCase):
     def test_interact(self):
-        a = Afumigatus(None, None, None, 100)
-        m = Macrophage(None, None, None)
-        tfbi = TransferrinBI(100)
-        tf = Transferrin(None, 100)
+        a = Afumigatus(100)
+        m = Macrophage()
+        tf = Transferrin(100, 100)
         iron = Iron(100)
 
-        tafcbi = Molecule(100)
-        tafc1 = TAFC(tafcbi, tf, 100)
-        tafc2 = TAFC(tafcbi, tf, 150)
+        tafc1 = TAFC(100, 100)
+        tafc2 = TAFC(150, 150)
 
         print("TAFC X TAFC")
-        print((tafc1.get(), tafc2.get()))
+        print((tafc1.getTafc(), tafc2.getTafc(), tafc1.getTafcbi(), tafc2.getTafcbi()))
         tafc1.interact(tafc2)
-        print((tafc1.get(), tafc2.get()))
+        print((tafc1.getTafc(), tafc2.getTafc(), tafc1.getTafcbi(), tafc2.getTafcbi()))
 
         print("A.fumitatus X TAFC")
-        print((a.iron_pool, tafc1.get()))
+        print((a.iron_pool, tafc1.getTafc(), tafc1.getTafcbi()))
         a.interact(tafc1)
-        print((a.iron_pool, tafc1.get()))
+        print((a.iron_pool, tafc1.getTafc(), tafc1.getTafcbi()))
 
         print("macrophage X TAFC")
-        print((m.iron_pool, tafc1.get()))
+        print((m.iron_pool, tafc1.getTafc(), tafc1.getTafcbi()))
         m.interact(tafc1)
-        print((m.iron_pool, tafc1.get()))
+        print((m.iron_pool, tafc1.getTafc(), tafc1.getTafcbi()))
 
         print("TfBI X TAFC")
-        print((tafc1.get(), tfbi.get()))
-        tafcbi = tfbi.interact(tafc1)
-        print((tafc1.get(), tfbi.get(), tafcbi.get()))
-
-        print("Tf X TAFC")
-        print((tafc1.get(), tf.get()))
+        print((tafc1.getTafc(), tafc1.getTafcbi(), tf.getTf(), tf.getTfBI()))
         tf.interact(tafc1)
-        print((tafc1.get(), tf.get()))
+        print((tafc1.getTafc(), tafc1.getTafcbi(), tf.getTf(), tf.getTfBI()))
 
         print("IRON X TAFC")
-        print((tafc1.get(), iron.get()))
+        print((tafc1.getTafc(), tafc1.getTafcbi(), iron.get()))
         iron.interact(tafc1)
-        print((tafc1.get(), iron.get()))
+        print((tafc1.getTafc(), tafc1.getTafcbi(), iron.get()))
